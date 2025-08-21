c5c0fd6c287a3c917eb93801e8719412a6b779df
Ninh Văn Long <ninhvanlong2105@gmail.com>
2025-08-21 14:15:29 +0700

    Controllers

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e873779cad94bc33b2e33a7dd715d10eba154be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+893a4409d9570850b7aa0233a6e83d0cefd3dba6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
